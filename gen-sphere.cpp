1defb2351bf15b44a0921abbb7f9d1356f1397a6
Neeraj Samtani <neerajjsamtani@gmail.com>
2019-12-15 00:16:45 -0800

    Allow multiple objects

--- gen-sphere.cpp
+++ gen-sphere.cpp
@@ -1,46 +1,25 @@
 #include <iostream>
-#include "vec3.h"
-#include "ray.h"
-
-// Find where the ray hit the sphere
-float hit_sphere(const vec3& center, float radius, const ray& r)
-{
-	// Form a quadratic equation to check if the ray satisfies
-	// x*x + y*y + z*z = R*R
-	// which in vector mathematics is
-	// dot((p(t)-c),(p(t)-c))=R*R
-	vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius*radius;
-	float discriminant = b*b - 4*a*c;
-	// Return the intersection of the ray and the sphere
-	if (discriminant < 0)
-	{
-		return -1.0;
-	}
-	else
-	{
-		return (-b - sqrt(discriminant)) / (2.0*a);
-	}
-}
+#include "sphere.h"
+#include "hitable_list.h"
+#include <limits>
 
 // Determine the color of the pixel
-vec3 color(const ray& r)
+vec3 color(const ray& r, hitable *world)
 {
+	hit_record rec;
 	// If the ray hits the sphere, find the normal to the sphere
 	// and assign a color according to its direction
-	float t = hit_sphere(vec3(0,0,-1), 0.5, r);
-	if (t > 0.0)
+	if (world->hit(r, 0.0, std::numeric_limits<float>::max(), rec))
 	{
-		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
-		return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+		return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
 	}
 	// Calculate a blue-white linear interpolation (gradient)
 	// Color the background based on this lerp
-	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5*(unit_direction.y() + 1.0);
-	return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+	else {
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5*(unit_direction.y() + 1.0);
+		return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+	}
 }
 
 // Print PPM to stdout
@@ -58,6 +37,11 @@ int main()
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
 	vec3 origin(0.0, 0.0, 0.0);
+	// Create two spheres and place them in the world
+	hitable *list[2];
+	list[0] = new sphere(vec3(0,0,-1), 0.5);
+	list[1] = new sphere(vec3(0, -100.5, -1), 100);
+	hitable *world = new hitable_list(list, 2);
 	// Color x axis from left to right using index i
 	// Color y axis from top to bottom using index j
 	for (int j = ny-1; j >= 0; j--)
@@ -67,7 +51,8 @@ int main()
 			float u = float(i) / float(nx);
 			float v = float(j) / float(ny);
 			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-			vec3 col = color(r);
+			vec3 p = r.point_at_parameter(2.0); // Why?
+			vec3 col = color(r, world);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
---
+++ hitable_list.h
@@ -0,0 +1,41 @@
+#ifndef HITABLELISTH
+#define HITABLELISTH
+
+#include "hitable.h"
+
+// Class for list of hitable items
+class hitable_list : public hitable
+{
+public:
+	hitable_list() {}
+	hitable_list(hitable **l, int n)
+	{
+		list = l;
+		list_size = n;
+	}
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
+	// List of pointers to hitable item pointers
+	hitable **list;
+	int list_size;
+};
+
+bool hitable_list::hit(const ray& r, float t_min, float t_max, hit_record& rec) const
+{
+	hit_record temp_rec;
+	bool hit_anything = false;
+	double closest_so_far = t_max
+	// For every object in the list
+	for (int i = 0; i < list_size; i++)
+	{
+		// Find the closest item the ray r hit
+		if(list[i]->hit(r, t_min, closest_so_far, temp_rec))
+		{
+			hit_anything = true;
+			closest_so_far = temp_rec.t;
+			rec = temp_rec;
+		}
+	}
+	return hit_anything;
+}
+
+#endif
